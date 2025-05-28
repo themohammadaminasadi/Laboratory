6ab96c1037ed341cd2d93755937674cb353b6c99
amin <asadi@ayandehsazfund.com>
2025-05-28 17:01:05 +0330

    Create Form Edit Result

---
+++ Laboratory/frmEditResultTest.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Laboratory
+{
+    partial class frmEditResultTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.faDatePicker1 = new FarsiLibrary.Win.Controls.FADatePicker();
+            this.lstPatient = new System.Windows.Forms.ListBox();
+            this.txtPatient = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblErrorMobile = new System.Windows.Forms.Label();
+            this.lblErrorNationalCode = new System.Windows.Forms.Label();
+            this.txtMobileNumber = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtNationalCode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // faDatePicker1
+            // 
+            this.faDatePicker1.Location = new System.Drawing.Point(632, 64);
+            this.faDatePicker1.Name = "faDatePicker1";
+            this.faDatePicker1.Size = new System.Drawing.Size(200, 20);
+            this.faDatePicker1.TabIndex = 67;
+            // 
+            // lstPatient
+            // 
+            this.lstPatient.FormattingEnabled = true;
+            this.lstPatient.ItemHeight = 24;
+            this.lstPatient.Location = new System.Drawing.Point(1025, 92);
+            this.lstPatient.Name = "lstPatient";
+            this.lstPatient.Size = new System.Drawing.Size(200, 172);
+            this.lstPatient.TabIndex = 66;
+            // 
+            // txtPatient
+            // 
+            this.txtPatient.Location = new System.Drawing.Point(1025, 62);
+            this.txtPatient.Name = "txtPatient";
+            this.txtPatient.Size = new System.Drawing.Size(200, 31);
+            this.txtPatient.TabIndex = 65;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("B Titr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(1231, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 29);
+            this.label1.TabIndex = 64;
+            this.label1.Text = "بیمار";
+            // 
+            // lblErrorMobile
+            // 
+            this.lblErrorMobile.AutoSize = true;
+            this.lblErrorMobile.Font = new System.Drawing.Font("B Nazanin", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblErrorMobile.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorMobile.Location = new System.Drawing.Point(764, 393);
+            this.lblErrorMobile.Name = "lblErrorMobile";
+            this.lblErrorMobile.Size = new System.Drawing.Size(0, 19);
+            this.lblErrorMobile.TabIndex = 63;
+            // 
+            // lblErrorNationalCode
+            // 
+            this.lblErrorNationalCode.AutoSize = true;
+            this.lblErrorNationalCode.Font = new System.Drawing.Font("B Nazanin", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblErrorNationalCode.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorNationalCode.Location = new System.Drawing.Point(765, 317);
+            this.lblErrorNationalCode.Name = "lblErrorNationalCode";
+            this.lblErrorNationalCode.Size = new System.Drawing.Size(0, 19);
+            this.lblErrorNationalCode.TabIndex = 62;
+            // 
+            // txtMobileNumber
+            // 
+            this.txtMobileNumber.Location = new System.Drawing.Point(632, 206);
+            this.txtMobileNumber.Name = "txtMobileNumber";
+            this.txtMobileNumber.Size = new System.Drawing.Size(200, 31);
+            this.txtMobileNumber.TabIndex = 61;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("B Titr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label9.Location = new System.Drawing.Point(852, 205);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 29);
+            this.label9.TabIndex = 60;
+            this.label9.Text = "موبایل";
+            // 
+            // txtNationalCode
+            // 
+            this.txtNationalCode.Location = new System.Drawing.Point(632, 125);
+            this.txtNationalCode.Name = "txtNationalCode";
+            this.txtNationalCode.Size = new System.Drawing.Size(200, 31);
+            this.txtNationalCode.TabIndex = 59;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("B Titr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(844, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 29);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "کد ملی ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("B Titr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(858, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 29);
+            this.label2.TabIndex = 57;
+            this.label2.Text = "تاریخ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(552, 193);
+            this.dataGridView1.TabIndex = 68;
+            // 
+            // frmEditResultTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(247)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(1312, 615);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.faDatePicker1);
+            this.Controls.Add(this.lstPatient);
+            this.Controls.Add(this.txtPatient);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblErrorMobile);
+            this.Controls.Add(this.lblErrorNationalCode);
+            this.Controls.Add(this.txtMobileNumber);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtNationalCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "frmEditResultTest";
+            this.Text = "EditResultTest";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FarsiLibrary.Win.Controls.FADatePicker faDatePicker1;
+        private System.Windows.Forms.ListBox lstPatient;
+        private System.Windows.Forms.TextBox txtPatient;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblErrorMobile;
+        private System.Windows.Forms.Label lblErrorNationalCode;
+        private System.Windows.Forms.TextBox txtMobileNumber;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtNationalCode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
